a84689edc51a17c12549c7ace3b0eafa5a330e72
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-06-28 08:22:42 +0300

    Update

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
